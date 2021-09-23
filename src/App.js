ddd5559acdaa54af3648747e78b3001a97e2b3b2
Andrei Sokolov <54791739+Sokolovs86@users.noreply.github.com>
2021-09-23 14:51:48 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ul>
+        <li>aarwwwwww</li>
+        <li>ooooooooooooooooo</li>
+        <li>y</li>
+        <li>oooooooкакпкпooooooooooooooo</li>
+        <li>h</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
