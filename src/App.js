b66cd7752d706987a8bd951743034b38231f3ad1
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-09-23 12:09:19 +0200

    asd

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ul>
+        <li>r</li>
+        <li>t</li>
+        <li>y</li>
+        <li>gss</li>
+        <li>h</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
