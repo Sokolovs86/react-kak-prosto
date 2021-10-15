e6f2596f23a0e578638968c6db6d20dcf733700d
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-15 09:16:30 +0200

    aq

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ const App = () => {
   return (
     <div className="App">
       <ul>
-        <li>111111111111111</li>
-        <li>ooooooooooooooooo</li>
-        <li>yffffffffffffffff</li>
+        <li>111111116666661111111</li>
+        <li>ooooooееееooooooo</li>
+        <li>yfffffwwwwwwwwwwfffffffffff</li>
         <li>oooooooкакпкпooooooooooooooo</li>
         <li>h</li>
       </ul>
