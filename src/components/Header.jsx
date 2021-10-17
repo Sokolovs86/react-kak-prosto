8fd059415ad0f4b6935ab4786eec2268269f7a8d
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-17 11:25:46 +0200

    add f2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Header = () => {
     return (
-      <header className='header'>
-        <img src='https://w7.pngwing.com/pngs/64/56/png-transparent-warner-bros-studio-tour-hollywood-warner-bros-studios-burbank-film-business-business-emblem-text-label-thumbnail.png'
-          alt='' />
+      <header className="header">
+        <img
+          src="https://i.ya-webdesign.com/images/the-internet-png-2.png"
+          alt=""
+        />
       </header>
     );
 }
