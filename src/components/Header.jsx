2e2c9d52f20741e81ef9b9a1b6e8a0671c078da6
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-17 09:04:22 +0200

    add n

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+      <header className='header'>
+        <img src='https://w7.pngwing.com/pngs/64/56/png-transparent-warner-bros-studio-tour-hollywood-warner-bros-studios-burbank-film-business-business-emblem-text-label-thumbnail.png'
+          alt='' />
+      </header>
+    );
+}
+
+export default Header;
