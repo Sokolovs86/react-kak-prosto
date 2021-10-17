ebc2b11aef5fb43f7d3c587431ba26b64b579a44
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-17 09:35:16 +0200

    add та

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src='https://w7.pngwing.com/pngs/64/56/png-transparent-warner-bros-studio-tour-hollywood-warner-bros-studios-burbank-film-business-business-emblem-text-label-thumbnail.png' alt='' />
-      </header>
-      <nav className='nav'>
-        <div>
-          <a href='#'>Profile</a>
-        </div>
-        <div>
-          <a href='#'>Messages</a>
-        </div>
-        <div>
-          <a href='#'>News</a>
-        </div>
-        <div>
-          <a href='#'>Music</a>
-        </div>
-        <div>
-          <a href='#'>Settings</a>
-        </div>
-      </nav>
-      <div className='content'>
-        <div>
-          <img src='https://pbs.twimg.com/media/E_NfXvgX0AEN1sc.jpg' alt='' />
-        </div>
-        <div>
-          ava + discription
-        </div>
-        <div>
-          My posts
-          <div>
-            New post
-          </div>
-            <div>
-              <div>
-                post 1
-              </div>
-              <div>
-                post 2
-              </div>
-            </div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://pbs.twimg.com/media/E_NfXvgX0AEN1sc.jpg" alt="" />
+      </div>
+      <div>ava + discription</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
