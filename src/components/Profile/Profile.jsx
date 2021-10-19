807708c52cbb1e68a5d600639553e68b60a61382
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-19 10:34:50 +0200

    add

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,16 +7,11 @@ const Profile = () => {
       <div>
         <img src="https://pbs.twimg.com/media/E_NfXvgX0AEN1sc.jpg" alt="" />
       </div>
-      <div>ava + discription</div>
       <div>
-        My posts
-        <div>New post</div>
-        <div className={ss.posts}>
-          <div className={ss.item}>post 1</div>
-          <div className={ss.item}>post 2</div>
-        </div>
+        ava + discription
       </div>
     </div>
+    
   );
 }
 
