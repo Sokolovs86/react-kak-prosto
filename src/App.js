5a6a0fb1fd146459a3158cd650efc7ad07a62bf6
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-21 12:46:21 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
+import React from "react";
+import "./App.css";
+import Dialogs from "./components/Dialogs/Dialogs";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
+    <div className="app-wrapper">
       <Header />
       <Navbar />
-      <Profile />
+      <div class="app-wrapper-content">
+        <Dialogs />
+      </div>
+      {/* <Profile /> */}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import ss from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className={ss.content}>
+    <div>
       <div>
         <img src="https://pbs.twimg.com/media/E_NfXvgX0AEN1sc.jpg" alt="" />
       </div>
