cb8abd60e5886e6bec9d2c22afcd05e4df52ab5e
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-22 11:57:09 +0200

    add

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,30 +3,30 @@ import ss from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return (
-    <div className="dialogs">
-      <div className="dialogs-items">
-        <div className="dialog">
+    <div className={ss.dialogs}>
+      <div className={ss.dialogsItems}>
+        <div className={ss.dialog}>
           Andrei
         </div>
-        <div className="dialog">
+        <div className={ss.dialog}>
           Dima
         </div>
-        <div className="dialog">
+        <div className={ss.dialog}>
           Artem
         </div>
-        <div className="dialog">
+        <div className={ss.dialog}>
           Lev
         </div>
-        <div className="dialog">
+        <div className={ss.dialog}>
           Petr
         </div>
       </div>
-    <div className="messages">
-      <div className="message">Hi</div>
-      <div className="messags">Hi Hi</div>
-      <div className="message">Hello</div>
-      <div className="message">Hello pa</div>
-      <div className="message">Hello ma</div>
+    <div className={ss.messages}>
+      <div className={ss.message}>Hi</div>
+      <div className={ss.message}>Hi Hi</div>
+      <div className={ss.message}>Hello</div>
+      <div className={ss.message}>Hello pa</div>
+      <div className={ss.message}>Hello ma</div>
     </div>
     </div>
   );
