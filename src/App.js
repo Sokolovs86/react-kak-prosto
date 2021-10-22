d3806878114a421221b9bb7a3fbe821248fa0a81
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-22 08:45:31 +0200

    add

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div class="app-wrapper-content">
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div class="app-wrapper-content">
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/news" component={News} />
+        </div>
       </div>
-      {/* <Profile /> */}
-    </div>
+    </BrowserRouter>
   );
 };
 
