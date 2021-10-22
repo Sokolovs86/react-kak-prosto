94aed7f047f6473e4edfea3f257c35048a3950ce
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-22 11:03:25 +0200

    add

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,9 +3,32 @@ import ss from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return (
-  <div>
-    Dialogs
-  </div>
+    <div className="dialogs">
+      <div className="dialogs-items">
+        <div className="dialog">
+          Andrei
+        </div>
+        <div className="dialog">
+          Dima
+        </div>
+        <div className="dialog">
+          Artem
+        </div>
+        <div className="dialog">
+          Lev
+        </div>
+        <div className="dialog">
+          Petr
+        </div>
+      </div>
+    <div className="messages">
+      <div className="message"></div>
+      <div className="messags"></div>
+      <div className="message"></div>
+      <div className="message"></div>
+      <div className="message"></div>
+    </div>
+    </div>
   );
 };
 
