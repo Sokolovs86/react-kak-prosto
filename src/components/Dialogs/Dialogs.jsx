beddd4df84f1d2f54bdbb3ca2f94fa11a1c35e12
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-22 13:17:25 +0200

    add

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -13,21 +13,28 @@ const DialogItem = (props) => {
   );
 }
 
+const Message = (props) => {
+   return <div className={ss.message}>{props.message}</div>;
+}
+
 const Dialogs = (props) => {
   return (
     <div className={ss.dialogs}>
       <div className={ss.dialogsItems}>
-
-        <DialogItem name='Andrei' id='1' />
-        <DialogItem name='Dima' id='2' />
-        <DialogItem name='Artem' id='3' />
-        <DialogItem name='Lev' id='4' />
-        <DialogItem name='Petr' id='5' />
+        <DialogItem name="Andrei" id="1" />
+        <DialogItem name="Dima" id="2" />
+        <DialogItem name="Artem" id="3" />
+        <DialogItem name="Lev" id="4" />
+        <DialogItem name="Petr" id="5" />
       </div>
       <div className={ss.messages}>
-        <div className={ss.message}>Hi</div>
-        <div className={ss.message}>Hi Hi</div>
-        <div className={ss.message}>Hello</div>
+        <Message message="Hi" />
+        <Message message="Hi Hi" />
+        <Message message="Hello" />
+        <Message message="Hi" />
+        <Message message="Hi" />
+        <div className={ss.message}></div>
+        <div className={ss.message}></div>
         <div className={ss.message}>Hello pa</div>
         <div className={ss.message}>Hello ma</div>
       </div>
