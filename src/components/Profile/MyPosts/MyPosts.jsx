34f47c25f4307b16a9af7e7784a9a74c23730cc2
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-23 09:27:32 +0200

    add

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import ss from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+  let postsData = [
+    { id: 1, post: "Hi, how are you?", likeCount: 15 },
+    { id: 2, post: "Cool, I'am the best", likeCount: 20 },
+  ];
+
   return (
     <div className={ss.postsBlock}>
       <h3>My posts</h3>
@@ -15,11 +21,13 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={ss.posts}>
-        <Post likesCount="15" message={"Hi, how are you?"} />
+        <Post post={postsData[0].post} likesCount={postsData[0].likeCount} id={postsData[0].id} />
+        <Post post={postsData[1].post} likesCount={postsData[1].likeCount} id={postsData[1].id} />
+        {/* <Post likesCount="15" message={"Hi, how are you?"} />
         <Post likesCount={20} message={"Cool, I'am the best"} />
         <Post message={"Help me"} likesCount={10} />
         <Post message="Yo" likesCount="15" />
-        <Post likesCount={20} />
+        <Post likesCount={20} /> */}
       </div>
     </div>
   );
