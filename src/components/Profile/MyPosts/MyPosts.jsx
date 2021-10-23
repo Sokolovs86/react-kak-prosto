3410526bd55b47c0f3b62f56440c50a82ffc339f
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-23 11:10:00 +0200

    add

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,10 +4,14 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
 
-  let postsData = [
+  let posts = [
     { id: 1, message: "Hi, how are you?", likesCount: 15 },
     { id: 2, message: "Cool, I'am the best", likesCount: 20 },
   ];
+
+  let postsElements = posts.map((p) => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
 
   return (
     <div className={ss.postsBlock}>
