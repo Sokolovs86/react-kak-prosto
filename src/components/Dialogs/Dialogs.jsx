7a5649b3f29dd6647c100dd261e0ed5b0b44a224
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-25 11:29:37 +0200

    add28

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,28 +5,9 @@ import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
-  let dialogsData = [
-    {id: 1, name: 'Andrei'}, 
-    {id: 2, name: 'Dima'}, 
-    {id: 3, name: 'Artem'}, 
-    {id: 4, name: 'Lev'}, 
-    {id: 5, name: 'Petr'}, 
-  ];
+  let dialogsElements = props.dialogsData.map( d => <DialogItem name={d.name} id={d.id} /> );
 
-  let messages = [
-    { id: 1, message: "Hi" },
-    { id: 2, message: "Hi Hi" },
-    { id: 3, message: "Hello" },
-    { id: 4, message: "Hello pa" },
-    { id: 5, message: "Yo" },
-  ];
-
-  let dialogsElements = dialogsData
-  .map( d => <DialogItem name={d.name} id={d.id} /> );
-
-  let messagesElements = messages.map((m) => (
-    <Message message={m.message} />
-  ));
+  let messagesElements = props.messages.map((m) => (<Message message={m.message} />));
 
     // let d= {
     //   id: 2,
--- src/index.js
+++ src/index.js
@@ -9,7 +9,23 @@ let posts = [
   { id: 2, message: "Cool, I'am the best", likesCount: 20 },
 ];
 
-ReactDOM.render(<App posts={posts} />, document.getElementById('root'));
+let dialogsData = [
+  { id: 1, name: "Andrei" },
+  { id: 2, name: "Dima" },
+  { id: 3, name: "Artem" },
+  { id: 4, name: "Lev" },
+  { id: 5, name: "Petr" },
+];
+
+let messages = [
+  { id: 1, message: "Hi" },
+  { id: 2, message: "Hi Hi" },
+  { id: 3, message: "Hello" },
+  { id: 4, message: "Hello pa" },
+  { id: 5, message: "Yo" },
+];
+
+ReactDOM.render(<App posts={posts} dialogsData={dialogsData} messages={messages} />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
