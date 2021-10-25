b971465ded6e17603f209ab302dbbc207a4cc2ec
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-25 12:53:05 +0200

    add29

--- src/App.js
+++ src/App.js
@@ -32,8 +32,8 @@ const App = (props) => {
           {/* <Route path="/dialogs" component={() => SomeComponent} /> */}
           {/* <Route path="/dialogs" render={() => <Dialogs />} /> */} 
           {/* <Route exact path="/dialogs"><Dialogs /></Route> */}
-          <Route path="/dialogs" render={() => <Dialogs dialogs={props.dialogs} messages={props.messages} />} />
-          <Route path="/profile" render={() => <Profile posts={props.posts} />} />
+          <Route path="/dialogs" render={() => <Dialogs dialogs={props.appState.dialogs} messages={props.appState.messages} />} />
+          <Route path="/profile" render={() => <Profile posts={props.appState.posts} />} />
           {/* <Route path="/dialogs" render={() => <News />} />
           <Route path="/dialogs" render={() => <Music />} />
           <Route path="/dialogs" render={() => <Settings />} /> */}
