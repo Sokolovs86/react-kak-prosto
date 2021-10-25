d67e5d3da0a6f72d71691c40f5159c733d3fe47f
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-25 10:58:59 +0200

    add28

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,6 +3,11 @@ import MyPosts from "./MyPosts/MyPosts";
 import ss from "./Profile.module.css";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
+let posts = [
+  { id: 1, message: "Hi, how are you?", likesCount: 15 },
+  { id: 2, message: "Cool, I'am the best", likesCount: 20 },
+];
+
 const Profile = () => {
   return (
     <div>
