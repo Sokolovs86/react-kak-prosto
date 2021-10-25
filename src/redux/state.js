76545b42b4e06d2ddcc5adf79e7dfa36faec3128
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-25 12:41:42 +0200

    add29

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,20 +1,22 @@
-posts = [
+let state = {
+posts: [
     { id: 1, message: "Hi, how are you?", likesCount: 15 },
     { id: 2, message: "Cool, I'am the best", likesCount: 20 },
-];
+],
 
-dialogs = [
+dialogs: [
     { id: 1, name: "Andrei" },
     { id: 2, name: "Dima" },
     { id: 3, name: "Artem" },
     { id: 4, name: "Lev" },
     { id: 5, name: "Petr" },
-];
+],
 
-messages = [
+messages: [
     { id: 1, message: "Hi" },
     { id: 2, message: "Hi Hi" },
     { id: 3,   message: "Hello" },
     { id: 4,   message: "Hello pa" },
     { id: 5,   message: "Yo" },
-];  
+], 
+}
