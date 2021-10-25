8b1400839a80a03de9a34f7bc5c9f5b02c013c9e
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-25 11:17:17 +0200

    add28

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let posts = [
+  { id: 1, message: "Hi, how are you?", likesCount: 15 },
+  { id: 2, message: "Cool, I'am the best", likesCount: 20 },
+];
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
