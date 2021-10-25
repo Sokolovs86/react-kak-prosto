a452d3348ea683960df5f3ce4d8d28b0d8e3ccac
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-25 13:09:34 +0200

    add29

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,24 +1,29 @@
 let state = {
-posts: [
-    { id: 1, message: "Hi, how are you?", likesCount: 15 },
-    { id: 2, message: "Cool, I'am the best", likesCount: 20 },
+    profilePage: {
+        posts: [
+            { id: 1, message: "Hi, how are you?", likesCount: 15 },
+            { id: 2, message: "Cool, I'am the best", likesCount: 20 },
 ],
 
-dialogs: [
-    { id: 1, name: "Andrei" },
-    { id: 2, name: "Dima" },
-    { id: 3, name: "Artem" },
-    { id: 4, name: "Lev" },
-    { id: 5, name: "Petr" },
+        dialogs: [
+            { id: 1, name: "Andrei" },
+            { id: 2, name: "Dima" },
+            { id: 3, name: "Artem" },
+            { id: 4, name: "Lev" },
+            { id: 5, name: "Petr" },
 ],
+}, 
 
-messages: [
+
+messagesPage: {
+    messages: [
     { id: 1, message: "Hi" },
     { id: 2, message: "Hi Hi" },
     { id: 3,   message: "Hello" },
     { id: 4,   message: "Hello pa" },
     { id: 5,   message: "Yo" },
 ], 
+}
 }
 
 export default state;
