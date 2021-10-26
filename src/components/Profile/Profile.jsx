019edc9e295264d9752a8cfb64b960d24b58e39b
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-26 08:16:31 +0200

    add29

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,6 +9,7 @@ import ProfileInfo from "./ProfileInfo/ProfileInfo";
 ]; */
 
 const Profile = (props) => {
+  // debugger;
   return (
     <div>
       <ProfileInfo />
--- src/redux/state.js
+++ src/redux/state.js
@@ -6,8 +6,8 @@ let state = {
     ],
     },
 
-dialogsPage: {
-    messages: [
+    dialogsPage: {
+        messages: [
         { id: 1, message: "Hi" },
         { id: 2, message: "Hi Hi" },
         { id: 3, message: "Hello" },
@@ -15,13 +15,18 @@ dialogsPage: {
         { id: 5, message: "Yo" },
     ],
 
-    dialogs: [
+        dialogs: [
         { id: 1, name: "Andrei" },
         { id: 2, name: "Dima" },
         { id: 3, name: "Artem" },
         { id: 4, name: "Lev" },
         { id: 5, name: "Petr" },
     ],
+    },
+    sidebar: {
+        sidebarPage: [ { 
+            
+        }]
     },
 };
 
