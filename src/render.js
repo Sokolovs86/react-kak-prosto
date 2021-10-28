ce93be6e1fd502f7b8f092bfad364b9565a648cf
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-28 11:18:39 +0200

    add34

--- src/render.js
+++ src/render.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { addPost } from "./redux/state";
+import { addPost, updateNewPostText } from "./redux/state";
 import { BrowserRouter } from "react-router-dom";
 
 export let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
-      <App state={state} addPost={ addPost} /> 
+      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} /> 
       {/* addPost={ (message) => { alert("IIIIUUUUUU" + message)}} /> тупая компонента */}
     </BrowserRouter>,
     document.getElementById("root")
