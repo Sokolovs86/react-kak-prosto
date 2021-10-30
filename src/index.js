11b4a3552ae819c614d1292c6832a96678456f92
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-30 08:55:03 +0200

    add35

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { rerenderEntireTree } from './render';
 import state from './redux/state';
 import { addPost, addPosts, updateNewPostText } from "./redux/state";
 import { BrowserRouter } from "react-router-dom";
