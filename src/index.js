40226fcf75c8641ea75ff778fde63caa024623b0
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-10-30 08:54:21 +0200

    add35

--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { rerenderEntireTree } from './render';
 import state from './redux/state';
+import { addPost, addPosts, updateNewPostText } from "./redux/state";
+import { BrowserRouter } from "react-router-dom";
+
+export let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <BrowserRouter>
+      <App
+        state={state}
+        addPost={addPost}
+        updateNewPostText={updateNewPostText}
+      />
+      {/* addPost={ (message) => { alert("IIIIUUUUUU" + message)}} /> тупая компонента */}
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
+};
 
 // import Dialogs from './components/Dialogs/Dialogs';
 
