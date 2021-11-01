a3dfffa6746a93ed52748de0bdd813a4ed7ece48
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-01 09:24:49 +0100

    add38

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,60 +1,60 @@
 let store = {
-    _state: {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi, how are you?", likesCount: 15 },
-      { id: 2, message: "Cool, I'am the best", likesCount: 20 },
-    ],
-    newPostText: "it-kabzda", //"it-kabzda"
-  },
-
-  dialogsPage: {
-    messages: [
-      { id: 1, message: "Hi" },
-      { id: 2, message: "Hi Hi" },
-      { id: 3, message: "Hello" },
-      { id: 4, message: "Hello pa" },
-      { id: 5, message: "Yo" },
-    ],
-
-    dialogs: [
-      { id: 1, name: "Andrei" },
-      { id: 2, name: "Dima" },
-      { id: 3, name: "Artem" },
-      { id: 4, name: "Lev" },
-      { id: 5, name: "Petr" },
-    ],
-  },
-  sidebar: {
-    sidebarPage: [{}],
-  },
-},
-    getState() {
-        return this._state;
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how are you?", likesCount: 15 },
+        { id: 2, message: "Cool, I'am the best", likesCount: 20 },
+      ],
+      newPostText: "it-kabzda", //"it-kabzda"
     },
-    _callSubscriber ()  {
-    console.log ('state changed');
-},
-    addPost ()  {
+
+    dialogsPage: {
+      messages: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "Hi Hi" },
+        { id: 3, message: "Hello" },
+        { id: 4, message: "Hello pa" },
+        { id: 5, message: "Yo" },
+      ],
+
+      dialogs: [
+        { id: 1, name: "Andrei" },
+        { id: 2, name: "Dima" },
+        { id: 3, name: "Artem" },
+        { id: 4, name: "Lev" },
+        { id: 5, name: "Petr" },
+      ],
+    },
+    sidebar: {
+      sidebarPage: [{}],
+    },
+  },
+  getState() {
+    return this._state;
+  },
+  _callSubscriber() {
+    console.log("state changed");
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer; // паттерн observer addEventListener похож на publisher-subscriber
+  },
+  
+  addPost() {
     let newPost = {
-        id: 3,
-        message: this._state.profilePage.newPostText,
-        likesCount: 25,
+      id: 3,
+      message: this._state.profilePage.newPostText,
+      likesCount: 25,
     };
 
     this._state.profilePage.posts.push(newPost);
-    this._state.profilePage.newPostText = '';
+    this._state.profilePage.newPostText = "";
     this._callSubscriber(this._state);
-},
-    updateNewPostText (newText)  {
+  },
+  updateNewPostText(newText) {
     this._state.profilePage.newPostText = newText;
     this._callSubscriber(this._state);
-    },
-    subscribe (observer)  {
-    this._callSubscriber = observer; // паттерн observer addEventListener похож на publisher-subscriber
-},
-
-}
+  },
+};
 
 export default store;
 window.state = store;
