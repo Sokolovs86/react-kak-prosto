884a211b5fa97e11552950a0fde77528418469d3
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-02 08:41:15 +0100

    add39

--- src/redux/state.js
+++ src/redux/state.js
@@ -60,5 +60,18 @@ let store = {
   },
 };
 
+export const addPostActionCreator = () => {
+  return {
+    type: "ADD-POST",
+  };
+};
+
+export const updateNewPostTextActionCreator = (text) => {
+  return {
+    type: "UPDATE-NEW-POST-TEXT",
+    newText: text,
+  };
+};
+
 export default store;
 window.store = store;
