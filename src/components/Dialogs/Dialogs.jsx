b15fa9c371f0518081e4fbc07c108dc63237bde8
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-02 14:45:05 +0100

    add40

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,9 +6,11 @@ import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
-let dialogsElements = props.state.dialogs.map( d => <DialogItem name={d.name} id={d.id} /> );
-let messagesElements = props.state.messages.map((m) => (<Message message={m.message} />));
-let newMessageBody = props.state.newMessageBody;
+let state = props.store.getState().dialogsPage;
+
+let dialogsElements = state.dialogs.map( d => <DialogItem name={d.name} id={d.id} /> );
+let messagesElements = state.messages.map((m) => (<Message message={m.message} />));
+let newMessageBody = state.newMessageBody;
 
 let onSendMessageClick = () => {
   props.store.dispatch(sendMessageCreator());
