bebeb6154230a7d658514ebf0f16e7d7d43b8271
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-03 09:25:07 +0100

    add41

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,5 +1,23 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+
 const profileReducer = (state, action) => {
-   
-   
-    return state;
+  if (action.type === ADD_POST) {
+    // this._addPost ();
+    let newPost = {
+      id: 3,
+      message: state.newPostText,
+      likesCount: 25,
+    };
+
+    state.posts.push(newPost);
+    state.newPostText = "";
+  } else if (action.type === UPDATE_NEW_POST_TEXT) {
+    // this._updateNewPostText (action.newText);
+    state.newPostText = action.newText;
+  }
+
+  return state;
 };
+
+export default profileReducer;
