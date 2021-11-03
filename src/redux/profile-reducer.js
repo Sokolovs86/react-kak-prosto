3a9ddea1ee1e24b0247764f8104da7a3ccdef767
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-03 09:54:09 +0100

    add41

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -2,22 +2,22 @@ const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
 const profileReducer = (state, action) => {
-  if (action.type === ADD_POST) {
-    // this._addPost ();
-    let newPost = {
-      id: 3,
-      message: state.newPostText,
-      likesCount: 25,
-    };
-
-    state.posts.push(newPost);
-    state.newPostText = "";
-  } else if (action.type === UPDATE_NEW_POST_TEXT) {
-    // this._updateNewPostText (action.newText);
-    state.newPostText = action.newText;
+    switch(action.type) {
+        case ADD_POST:
+            let newPost = {
+                id: 3,
+                message: state.newPostText,
+                likesCount: 25,
+            };
+            state.posts.push(newPost);
+            state.newPostText = "";
+            return state;
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newText;
+            return state;
+        default:
+            return state;
   }
-
-  return state;
 };
 
 export default profileReducer;
