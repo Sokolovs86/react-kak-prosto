0a8c89ed7f06b6ab69fb89a32c8b1e111bf017e5
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-03 13:10:59 +0100

    addd42

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,13 @@
-import {createStore} from "redux";
+import {combineReducers, createStore} from "redux";
+import dialogsReducer from "./dialogs-reducer";
+import profileReducer from "./profile-reducer";
+import sidebarReducer from "./sidebar-reducer";
+
+let reducers = combineReducers({
+  dialogsReducer,
+  profileReducer,
+  sidebarReducer,
+});
 
 let store = createStore();
 
