8beeff8871d788d83a9c3c49e1820437368bc08e
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-04 13:17:00 +0100

    add43

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,54 +2,22 @@ import React from "react";
 import { addPostActionCreator, updateNewPostTextActionCreator } from "../../../redux/profile-reducer";
 import ss from "./MyPosts.module.css";
 import Post from "./Post/Post";
-//import {addPostActionCreator, updateNewPostTextActionCreator } from "../../../redux/state";
-
-// let addPostActionCreator = () => {
-//   return {
-//     type: 'ADD-POST',
-//   }
-// }
-
-// let updateNewPostTextActionCreator = (text) => {
-//   return {
-//     type: 'UPDATE-NEW-POST-TEXT',
-//     newText: text,
-//   };
-// };
-
-
 
 const MyPosts = (props) => {
-
- /*  let posts = [
-    { id: 1, message: "Hi, how are you?", likesCount: 15 },
-    { id: 2, message: "Cool, I'am the best", likesCount: 20 },
-  ]; */
 
 let postsElements = 
   props.posts.map((p) => (<Post message={p.message} likesCount={p.likesCount} />));
 
 let newPostElement = React.createRef();
 
-let addPost = () => {
-  //let text = newPostElement.current.value;
-  //props.addPost();
-  props.dispatch(addPostActionCreator() );
-  // newPostElement.current.value = ''; // занулить значение value 
-  // alert("google.com");
-//   let text = document.getElementById('new-post').value;
-//   alert(text);
+let onAddPost = () => {
+  props.addPost(); 
 }
 
 let onPostChange = () => {
   let text = newPostElement.current.value;
-  props.updateNewPostText (text);
-  //let action = ({ type: "UPDATE-NEW-POST-TEXT", newText: text });
-  //let action = updateNewPostTextActionCreator(text);
-  //props.dispatch (action);
-  //console.log (text);
+  props.updateNewPostText (text); 
 }
- 
   return (
     <div className={ss.postsBlock}>
       <h3>My posts</h3>
@@ -59,7 +27,7 @@ let onPostChange = () => {
           <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
         </div>
         <div>
-          <button onClick={addPost}>Add post</button>
+          <button onClick={onAddPost}>Add post</button>
         </div>
       </div>
       <div className={ss.posts}>
--- src/components/Profile/MyPosts/Post/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/Post/MyPostsContainer.jsx
@@ -1,57 +1,18 @@
 import React from "react";
 import { addPostActionCreator, updateNewPostTextActionCreator } from "../../../redux/profile-reducer";
-import ss from "./MyPosts.module.css";
-import Post from "./Post/Post";
-//import {addPostActionCreator, updateNewPostTextActionCreator } from "../../../redux/state";
-
-// let addPostActionCreator = () => {
-//   return {
-//     type: 'ADD-POST',
-//   }
-// }
-
-// let updateNewPostTextActionCreator = (text) => {
-//   return {
-//     type: 'UPDATE-NEW-POST-TEXT',
-//     newText: text,
-//   };
-// };
-
-
+import MyPosts from "../MyPosts";
 
 const MyPostsContainer = (props) => {
+  let addPost = () => {
+    props.dispatch(addPostActionCreator());
+  }
 
- /*  let posts = [
-    { id: 1, message: "Hi, how are you?", likesCount: 15 },
-    { id: 2, message: "Cool, I'am the best", likesCount: 20 },
-  ]; */
-
-let postsElements = 
-  props.posts.map((p) => (<Post message={p.message} likesCount={p.likesCount} />));
-
-let newPostElement = React.createRef();
-
-let addPost = () => {
-  //let text = newPostElement.current.value;
-  //props.addPost();
-  props.dispatch(addPostActionCreator() );
-  // newPostElement.current.value = ''; // занулить значение value 
-  // alert("google.com");
-//   let text = document.getElementById('new-post').value;
-//   alert(text);
-}
-
-let onPostChange = () => {
-  let text = newPostElement.current.value;
-  //props.updateNewPostText (text);
-  //let action = ({ type: "UPDATE-NEW-POST-TEXT", newText: text });
-  let action = updateNewPostTextActionCreator(text);
-  props.dispatch (action);
-  //console.log (text);
-}
+  let onPostChange = (text) => {
+    let action = updateNewPostTextActionCreator(text);
+    props.dispatch (action);
+  }
  
-  return (<MyPosts updateNewPostText = {() => {let action = updateNewPostTextActionCreator(text);
-  props.dispatch(action);}} />);
+  return <MyPosts updateNewPostText={onPostChange} addPost ={addPost} posts={props.posts} />;
 };
 
 export default MyPostsContainer;
