4fc3a72f174cd2d9b8a9d5e3ddc4749404f44047
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-05 12:15:23 +0100

    add44

--- src/index.js
+++ src/index.js
@@ -5,18 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/redux-store';
 import { BrowserRouter } from "react-router-dom";
+import StoreContext from './StoreContext';
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
+      <StoreContext.Provider>
       <App
         state={state}
         dispatch={store.dispatch.bind(store)}
         store={store} 
       />
       {/* addPost={ (message) => { alert("IIIIUUUUUU" + message)}} /> тупая компонента */}
-    </BrowserRouter>,
-    document.getElementById("root")
+    </StoreContext.Provider>
+    </BrowserRouter>, document.getElementById("root")
   );
 };
 
