41d781566f2e868301227ecb5bb07b30ad74fc9e
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-05 12:33:34 +0100

    add44

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
+import StoreContext from '../../../StoreContext';
 import MyPosts from "./MyPosts";
 
 
 const MyPostsContainer = (props) => {
-    debugger
-    let state = props.store.getState();
+ 
+    //let state = props.store.getState();
 
     let addPost = () => {
         props.store.dispatch(addPostActionCreator());
@@ -16,9 +17,17 @@ const MyPostsContainer = (props) => {
         props.store.dispatch(action);
     }
 
-    return (<MyPosts updateNewPostText={onPostChange} addPost={addPost}
-                     posts={state.profilePage.posts}
-                     newPostText={state.profilePage.newPostText} />)
+    return (
+      <StoreContext.Consumer> {
+          (store) => (
+        <MyPosts
+          updateNewPostText={onPostChange}
+          addPost={addPost}
+          posts={store.profilePage.posts}
+          newPostText={store.getState().profilePage.newPostText}/>)
+      }
+      </StoreContext.Consumer>
+    );
 }
 
 export default MyPostsContainer;
