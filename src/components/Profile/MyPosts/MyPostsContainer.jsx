f812ca28a8e12be89700030bc0b54009d03a6ad5
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-05 13:09:59 +0100

    add44

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -5,24 +5,24 @@ import MyPosts from "./MyPosts";
 
 const MyPostsContainer = (props) => {
     return (
-      <StoreContext.Consumer> {
-          (store) => {
-        let state = store.getState();
+      <StoreContext.Consumer> 
+        {
+        (store) => {
+            let state = store.getState();
 
-        let addPost = () => {
-          store.dispatch(addPostActionCreator());
-        };
+            let addPost = () => {
+            store.dispatch(addPostActionCreator());
+            };
 
-        let onPostChange = (text) => {
-          let action = updateNewPostTextActionCreator(text);
-          store.dispatch(action);
-        };
+            let onPostChange = (text) => {
+            let action = updateNewPostTextActionCreator(text);
+            store.dispatch(action);
+            };
 
-        return <MyPosts
-          updateNewPostText={onPostChange}
-          addPost={addPost}
-          posts={state.profilePage.posts}
-          newPostText={state.getState().profilePage.newPostText}/>}
+        return <MyPosts updateNewPostText={onPostChange}
+                        addPost={addPost}
+                        posts={state.profilePage.posts}
+                        newPostText={state.profilePage.newPostText}/>}
       }
       </StoreContext.Consumer>
     );
