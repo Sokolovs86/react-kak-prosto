ba9c0ff51514ddf8043a9930cfbdd7fe046d72eb
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-06 11:26:03 +0100

    add47

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -21,8 +21,7 @@ const mapDispatchToProps = (dispatch) => {
         }
     }
 }
-
-
+ 
 const MyPostsContainer = connect (mapStateToProps, mapDispatchToProps) (MyPosts);
 
 export default MyPostsContainer;
