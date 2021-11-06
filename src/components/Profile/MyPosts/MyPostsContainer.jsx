fb7a8eb439fb4cf306285a52414a8435f6277547
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-06 10:00:19 +0100

    add45

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -2,45 +2,28 @@ import React from 'react';
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 import StoreContext from '../../../StoreContext';
 import MyPosts from "./MyPosts";
+import {connect} from "react-redux";
 
-const MyPostsContainer = (props) => {
-    return (
-      <StoreContext.Consumer> 
-        {
-        (store) => {
-            let state = store.getState();
+ let mapStateToProps = (state) => {
+     return {
+        proposts: state.profilePage.posts,
+        newPostText: state.profilePage.newPostText,
+     };
+ };
 
-            let addPost = () => {
-            store.dispatch(addPostActionCreator());
-            };
-
-            let onPostChange = (text) => {
+ let mapDispatchToProps = (dispatch) => {
+     return {
+        updateNewPostText: (text) => {
             let action = updateNewPostTextActionCreator(text);
-            store.dispatch(action);
-            };
-
-        return <MyPosts updateNewPostText={onPostChange}
-                        addPost={addPost}
-                        posts={state.profilePage.posts}
-                        newPostText={state.profilePage.newPostText}/>}
-      }
-      </StoreContext.Consumer>
-    );
+            dispatch(action);
+        },
+        addPost: () => {
+            store.dispatch(addPostActionCreator());
+        }
+    }
 }
 
-// let mapStateToProps = (state) => {
-//     return {
-//         profilePage: state.profilePage
-//     };
-// };
 
-// let mapDispatchToProps = (dispatch) => {
-//     return {
-//       updateNewPostText: () => {
-//           dispatch(updateNewPostTextActionCreator());
-//       },
-
-//     };
-// };
+const MyPostsContainer = connect (mapStateToProps, mapDispatchToProps) (MyPosts);
 
 export default MyPostsContainer;
