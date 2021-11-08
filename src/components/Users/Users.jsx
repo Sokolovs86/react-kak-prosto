b82aa0d1a2f90520d2c29a34aa3a792301317529
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-08 14:53:35 +0100

    add49

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -44,7 +44,7 @@ let Users = (props) => {
     return <div>
         {
        props.users.map(u => <div key={u.id}>
-       <span>
+       <div>
            <div>
                <img src={u.photoUrl} className={styles.userPhoto} />
            </div>
@@ -53,17 +53,17 @@ let Users = (props) => {
              ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
              : <button onClick={() => {props.follow(u.id)}}>Follow</button>}   
            </div>
-       </span>
-       <span>
-           <span>
+       </div>
+       <div>
+           <div>
                <div>{u.fullName}</div>
                <div>{u.status}</div>
-           </span>
-           <span>
+           </div>
+           <div>
                <div>{u.location.city}</div>
                <div>{u.location.country}</div>
-           </span>
-       </span>
+           </div>
+       </div>
        </div>)
         }
     </div>
