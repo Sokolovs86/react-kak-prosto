e9411020028e86b11f67f0e322cb16a8eca8f490
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-24 11:19:41 +0500

    Update Users.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,9 +4,8 @@ import * as axios from 'axios';
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            
-        props.setUsers();
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => { 
+            props.setUsers(response.data.Items);
         });
 
 };
