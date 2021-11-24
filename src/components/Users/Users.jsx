a9db4f297a0eb32a698c4d8f010939e3f0a94694
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-24 10:52:41 +0500

    add51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,43 +4,9 @@ import styles from './users.module.css'
 let Users = (props) => {
     if (props.users.length === 0) {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers( [
-    {
-      id: 1,
-      photoUrl: "https://cs13.pikabu.ru/avatars/2729/x2729750-428926448.png",
-      followed: false,
-      fullName: "Dmitri D.",
-      status: "I'm boss",
-      location: { city: "Minsk", country: "Belarus" },
-    },
-    {
-      id: 2,
-      photoUrl: "https://cs13.pikabu.ru/avatars/2729/x2729750-428926448.png",
-      followed: true,
-      fullName: "Elena M.",
-      status: "I'm the best",
-      location: { city: "Moscow", country: "Russia" },
-    },
-    {
-      id: 3,
-      photoUrl: "https://cs13.pikabu.ru/avatars/2729/x2729750-428926448.png",
-      followed: false,
-      fullName: "Isabella F.",
-      status: "I'm cool",
-      location: { city: "Toronto", country: "Canada" },
-    },
-    {
-      id: 4,
-      photoUrl: "https://cs13.pikabu.ru/avatars/2729/x2729750-428926448.png",
-      followed: true,
-      fullName: "Peter G.",
-      status: "I'm hungry",
-      location: { city: "Oslo", country: "Sweden" },
-    },
-],
-    );
+            props.setUsers();
         });
-        
+
 };
 
 
