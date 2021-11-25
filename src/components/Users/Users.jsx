bd7ccfbd2ebc0d7725ff7a473521c89202fb9ce7
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-25 12:43:59 +0500

    add53

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -7,11 +7,10 @@ class Users extends React.Component {
 
   constructor(props) {
     super(props);
-    if (this.props.users.length === 0) {
+    
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => { 
         this.props.setUsers(response.data.items);
         });
-};
   }
 
   
