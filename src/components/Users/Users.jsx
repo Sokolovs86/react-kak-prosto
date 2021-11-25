a1e909bdb9ebe12851a997fd085d360cd4867be0
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-25 14:22:38 +0500

    addd55

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -17,7 +17,7 @@ class Users extends React.Component {
             <div>
                <span>1</span>
                <span>2</span>
-               <span>3</span>
+               <span className={styles.selectedPage}>3</span>
                <span>4</span>
                <span>5</span>
             </div>
