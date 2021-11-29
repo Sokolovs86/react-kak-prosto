60684cecc12e55f44cc9bed1c3d6c68621e4c2aa
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-29 15:14:46 +0500

    add57

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -36,17 +36,21 @@ class UsersContainer extends React.Component {
 
   render() {
     return (
-        <>
-          {this.props.isFetching ? <img src={preloader} alt={'downloading gif'}/> : null}
-      <Users
-        totalUsersCount={this.props.totalUsersCount}
-        pageSize={this.props.pageSize}
-        currentPage={this.props.currentPage}
-        onPageChanged={this.onPageChanged}
-        users={this.props.users}
-        follow={this.props.follow}
-        unfollow={this.props.unfollow}
-      />
+      <>
+        {this.props.isFetching ? (
+          <div style={{ width: "450px", height: "300px" }}>
+            <img src={preloader} alt={"downloading gif"} />
+          </div>
+        ) : null}
+        <Users
+          totalUsersCount={this.props.totalUsersCount}
+          pageSize={this.props.pageSize}
+          currentPage={this.props.currentPage}
+          onPageChanged={this.onPageChanged}
+          users={this.props.users}
+          follow={this.props.follow}
+          unfollow={this.props.unfollow}
+        />
       </>
     );
   }
