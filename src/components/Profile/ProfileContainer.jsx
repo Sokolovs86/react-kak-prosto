30067c7441cb7860d79e7774b1df13858210b01f
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-11-30 14:36:30 +0500

    add60

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Profile from './Profile';
-import {Axios as axios} from 'axios';
+import  * as axios from 'axios';
 import { connect } from 'react-redux';
-import {setUserProfile} from "../../redux/profile-reducer";
+import {setUserProfile} from '../../redux/profile-reducer';
+import { withRouter } from 'react-router-dom';
 
 class ProfileContainer extends React.Component {
 
     componentDidMount() {
+
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
             .then((response) => {
                 this.props.setUserProfile(response.data);
@@ -16,14 +18,16 @@ class ProfileContainer extends React.Component {
     render() {
         return (
             <div>
-                <Profile {...this.props} />
+                <Profile {...this.props} profile={this.props.profile} />
             </div>
         );
     }
 };
 
 let mapStateToProps = (state) => ({
-
+    profile: state.profilePage.profile,
 });
+
+withRouter(ProfileContainer);
 
 export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import Preloader from "../../common/Preloader/Preloader";
 import ss from "./ProfileInfo.module.css";
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+  if(!props.profile) {
+  //if (props.profile == null || props.profile === undefined) 
+    return <Preloader />
+  }
+
   return (
     <div>
       <div>
         <img src='https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg' />
       </div>
-      <div className={ss.descriptionBlock}>ava + description</div>
+      <div className={ss.descriptionBlock}>
+        <img src={props.profile.photos.large} alt='' />
+      </div>
     </div>
   );
 };
