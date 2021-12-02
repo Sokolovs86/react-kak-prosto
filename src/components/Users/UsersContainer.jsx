5d0a98d229a22d0434767383391d96ca5e915881
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-12-02 12:53:12 +0500

    add63

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -10,26 +10,24 @@ class UsersContainer extends React.Component {
   componentDidMount() {
     this.props.toggleIsFetching(true);
 
-    getUsers (this.props.currentPage, this.props.pageSize).then((response) => {
+    getUsers (this.props.currentPage, this.props.pageSize)
+        .then((data) => {
               this.props.toggleIsFetching(false);
-              this.props.setUsers(response.data.items);
-              this.props.setTotalUsersCount(response.data.totalCount);
+              this.props.setUsers(data.items);
+              this.props.setTotalUsersCount(data.totalCount);
       });
   }
 
   onPageChanged = (pageNumber) => {
     this.props.setCurrentPage(pageNumber);
     this.props.toggleIsFetching(true);
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
-        {
-          withCredentials: true,
-        }
-      )
-      .then((response) => {
+
+    getUsers(pageNumber, this.props.pageSize)
+      .then((data) => {
         this.props.toggleIsFetching(false);
-        this.props.setUsers(response.data.items);
+        this.props.setUsers(data.items);
       });
-  };
+    };
 
   render() {
     return (
