71c3eabe1c9ffa4a1db170c6ad5ae95fa9f8b3ee
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-12-02 11:56:59 +0500

    add63 first

--- src/api/api.js
+++ src/api/api.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import * as axios from "axios";
 
-const instance = axios.create({
-  withCredentials: true,
-  baseURL: "https://social-network.samuraijs.com/api/1.0/",
-  headers: {
-    "API-KEY": "513d06ec-0e54-4e4f-add7-d8a92a070409",
-  },
-});
+ axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
+   withCredentials: true,
+ });
 
-export const usersAPI = {
-  getUsers(currentPage = 1, pageSize = 10) {
-    return instance
-      .get(`users?page=${currentPage}&count=${pageSize}`)
-      .then((response) => {
-        return response.data;
-      });
-  },
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const instance = axios.create({
+//   withCredentials: true,
+//   baseURL: "https://social-network.samuraijs.com/api/1.0/",
+//   headers: {
+//     "API-KEY": "513d06ec-0e54-4e4f-add7-d8a92a070409",
+//   },
+// });
+
+// export const usersAPI = {
+//   getUsers(currentPage = 1, pageSize = 10) {
+//     return instance
+//       .get(`users?page=${currentPage}&count=${pageSize}`)
+//       .then((response) => {
+//         return response.data;
+//       });
+//   },
+// };
