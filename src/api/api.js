6fb1c09957be3ad9104374ca15c633afd3596cae
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-12-02 13:02:52 +0500

    add63

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import * as axios from 'axios';
 
+const baseURL = "https://social-network.samuraijs.com/api/1.0/";
+
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-  return axios.get(
-    `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
+  return axios.get(baseURL + `users?page=${currentPage}&count=${pageSize}`,
     {
       withCredentials: true,
     })
     .then(response => {
      return response.data
+    });
+};
+
+export const getUsers = (currentPage = 1, pageSize = 10) => {
+  return axios.get(baseURL + `follow?page=${currentPage}&count=${pageSize}`, {
+      withCredentials: true,
+    })
+    .then((response) => {
+      return response.data;
     });
 };
 
