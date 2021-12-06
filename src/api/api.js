c92a0bc37983b08cdc2c2f2d28ac06750546c23c
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-12-06 11:28:42 +0500

    add66

--- src/api/api.js
+++ src/api/api.js
@@ -18,10 +18,10 @@ export const  usersAPI = {
       });
   },
   follow(userId) {
-    return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}` )
+    return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}` )
   },
   unfollow(userId) {
-    return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}` )
+    return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}` )
   }
 }
 
