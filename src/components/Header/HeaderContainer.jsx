96f973e158b881e59718adf4a04609c5c13ac276
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-12-07 11:05:24 +0500

    add67

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import { connect } from 'react-redux';
-import { setAuthUserData } from '../../redux/auth-reducer';
+import { getAuthUserData } from '../../redux/auth-reducer';
 import {authAPI} from '../../api/api';
 
 class HeaderContainer extends React.Component {
     componentDidMount() {
-        authAPI.me()
-        .then((response) => {
-        if (response.data.resultCode === 0) {
-            let { id, login, email } = response.data.data;
-            this.props.setAuthUserData(id, email, login);
-            }
-        });
+        this.props.setAuthUserData();
     }
 
     render() {
@@ -24,4 +18,4 @@ const mapStateToProps = (state) => ({
     login: state.auth.login,
 });
 
-export default connect(mapStateToProps, { setAuthUserData })(HeaderContainer);
+export default connect(mapStateToProps, { getAuthUserData })(HeaderContainer);
