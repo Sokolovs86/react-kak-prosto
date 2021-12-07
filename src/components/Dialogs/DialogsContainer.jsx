053b879f059e3dd92e617c8b60a8ea796a0076fd
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-12-07 13:51:18 +0500

    add70

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -4,6 +4,7 @@ import Dialogs from './Dialogs';
 import {connect} from 'react-redux';
 import {withAuthRedirect} from '../../hoc/withAuthRedirect';
 import { Redirect } from "react-router-dom";
+import {compose} from "redux";
 
 let mapStateToProps = (state) => {
     return {
@@ -21,6 +22,11 @@ let mapDispatchToProps = (dispatch) => {
         },
     };
 };
+
+compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    withAuthRedirect
+) (Dialogs)
 
 let AuthRedirectComponent = withAuthRedirect (Dialogs);
 
