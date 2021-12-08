5610b3368a95c1310e30d81462805c34034cd853
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-12-08 11:20:23 +0500

    add71

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -8,10 +8,18 @@ class ProfileStatus extends React.Component {
         title: 'YOOOOOO',
     }
 
-    activateEditMode = () => {
+    activateEditMode ()  {
+       // console.log (this.state.editMode);
         this.setState({
             editMode: true,
-        })
+        });
+       // console.log (this.state.editMode);
+    };
+
+    deactivateEditMode ()  {
+        this.setState({
+            editMode: false,
+        });
     };
 
     render () {
@@ -19,12 +27,12 @@ class ProfileStatus extends React.Component {
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={ this.activateEditMode }>{this.props.status}</span>
+                        <span onDoubleClick={ this.activateEditMode.bind(this) }>{this.props.status}</span>
                     </div>
                 }
                 {this.state.editMode &&
                     <div>
-                        <input value={this.props.status}/>
+                        <input onBlur = {this.deactivateEditMode.bind(this)} value={this.props.status}/>
                     </div>
                 }
             </div>
