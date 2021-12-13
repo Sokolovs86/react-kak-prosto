0d062bcb7ab72f6bd7452933ef5d53997bf31c95
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-12-13 09:26:19 +0500

    add73

--- src/api/api.js
+++ src/api/api.js
@@ -24,22 +24,10 @@ export const usersAPI = {
   },
   getProfile(userId) {
     console.warn('Obsolete method. Please use profileAPI object');
-    return profileAPI.getProfile(userId);
+    return instance.getProfile(userId);
   }
 };
 
-export const profileAPI = {
-
-  getProfile(userId) {
-    return instance.get(`profile/` + userId);
-  },
-  getStatus(userId) {
-    return instance.get(`profile/status/` + userId);
-  },
-  updateStatus(status) {
-    return instance.put(`profile/status`, {status: status});
-  },
-};
 
 export const authAPI = {
   me() {
