aebabbf4045e36bfc85d52bec8e0f699f9e57926
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-12-14 15:19:22 +0500

    add76

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,48 +1,42 @@
-import React from "react";
-import ss from "./MyPosts.module.css";
-import Post from "./Post/Post";
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
+import React from 'react';
+import ss from './MyPosts.module.css';
+import Post from './Post/Post';
+import {Field, reduxForm} from "redux-form";
+
+let AddNewPostForm = (props) => {
+    return <form onSubmit={props.handleSubmit}>
+        <div>
+            <Field name="newPostText" component="textarea" />
+        </div>
+        <div>
+            <button>Add post</button>
+        </div>
+    </form>;
+}
+
+let AddNewPostFormRedux = reduxForm({form: "ProfileAddNewPostForm"})(AddNewPostForm);
 
 const MyPosts = (props) => {
+    let postsElements =
+        props.posts.map( p => <Post message={p.message} likesCount={p.likesCount}/>);
 
-let postsElements = 
-  props.posts.map((p) => (<Post message={p.message} likesCount={p.likesCount} />));
+    let newPostElement = React.createRef();
 
-let newPostElement = React.createRef();
+    let onAddPost = (values) => {
+        props.addPost(values.newPostText);
+    }
 
-// let onAddPost = () => {
-//   props.addPost(); 
-// }
- let onAddPost = () => {
-    props.addPost();
- }
-
-let onPostChange = () => {
-  let text = newPostElement.current.value;
-  props.updateNewPostText (text); 
+    return (
+        <div className={ss.postsBlock}>
+            <h3>My posts</h3>
+            <AddNewPostFormRedux onSubmit={onAddPost} />
+            <div className={ss.posts}>
+                {postsElements}
+            </div>
+        </div>
+    )
 }
-  return (
-    <div className={ss.postsBlock}>
-      <h3>My posts</h3>
-      <div>
-        <div>
-          {/* <textarea id='new-post'></textarea> */}
-          <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
-        </div>
-        <div>
-          <button onClick={onAddPost}>Add post</button>
-        </div>
-      </div>
-      <div className={ss.posts}>
-        {postsElements}
-        {/* <Post likesCount="15" message={"Hi, how are you?"} />
-        <Post likesCount={20} message={"Cool, I'am the best"} />
-        <Post message={"Help me"} likesCount={10} />
-        <Post message="Yo" likesCount="15" />
-        <Post likesCount={20} /> */}
-      </div>
-    </div>
-  );
-};
+
+
 
 export default MyPosts;
