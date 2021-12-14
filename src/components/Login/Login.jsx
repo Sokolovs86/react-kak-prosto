bbad1f0ac692c5b756216299c472f2514275d02e
Andrei Sokolov <sokolovsandrey@gmail.com>
2021-12-14 12:17:20 +0500

    add75

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import {reduxForm} from 'redux-form';
+import {Field, reduxForm} from 'redux-form';
 
 const LoginForm = (props) => {
     return (
-     <form>
+     <form onSubmit={props.handleSubmit}>
          <div>
-             <input placeholder={"Login"} />
+             <Field placeholder={"Login"} name={'login'} component={'input'} />
          </div>
          <div>
-             <input placeholder={"Password"} />
+             <Field placeholder={"Password"} name={'password'} component={'input'} />
          </div>
          <div>
-             <input type={"checkbox"} /> remember me
+             <Field component={'input'} name={'rememberMe'} type={"checkbox"} /> remember me
          </div>
          <div>
              <button>Login</button>
